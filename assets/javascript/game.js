f34a2044ba5f850e23be041cff5351a29528b37b
unknown <vleague2@gmail.com>
2018-03-30 19:52:28 -0400

    outline for JS logic

---
+++ assets/javascript/game.js
@@ -0,0 +1,63 @@
+// VARIABLES
+
+// String variable: yourTotal, which is tied to display h5 #your-total
+
+// String variable: winCounter set to 0, which is tied to display span #win-ct
+
+//String variable: lossCounter set to 0, which is tied to display span #loss-ct
+
+// Num variable: randomNumber, which is tied to display h5 #random-number
+
+// Num variable: pusheen1Value, which is tied to img #pusheen-1
+
+//Num variable: pusheen2Value, which is tied to img #pusheen-2
+
+// Num variable: pusheen3Value, which is tied to img #pusheen-3
+
+// Num variable: pusheen4Value, which is tied to img #pusheen-4
+
+
+
+// FUNCTIONS
+
+// generate randomNumber function
+
+// generate pusheen1Value function
+    // a number between X and Y 
+
+// generate pusheen2Value function
+    // a number between X and Y 
+
+// generate pusheen3Value function
+    // a number between X and Y
+
+// generate pusheen4Value function
+    // a number between X and Y
+
+// Restart game function
+    // Empty yourTotal
+    // generate new randomNumber
+    // display new randomNumber
+    // generate new values for Pusheens
+
+
+
+
+// GAME
+
+// Game generates randomNumber
+// Game generates values for Pusheens
+// Game displays randomNumber
+
+// If yourTotal < randomNumber
+    // .Click function to listen for clicks on the Pusheens
+
+    // The clicked Pusheen's value gets pushed to yourTotal
+
+// If yourTotal === randomNumber
+    // Add to winCounter
+    // Restart game
+
+// If yourTotal > randomNumber
+    // Add to lossCounter
+    // Restart game
